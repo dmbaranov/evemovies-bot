fef2a9165be90a2cdbabbf12b1a372b9c78cb8b6
Dmitrii Baranov <me@dmbaranov.io>
2018-12-06 13:39:50 +0200

    Fixed wrapper function to work with webHooks

--- src/util/error-handler.ts
+++ src/util/error-handler.ts
@@ -1,13 +1,19 @@
 import { ContextMessageUpdate } from 'telegraf';
 import logger from './logger';
 
+/**
+ * Wrapper to catch async errors within a stage. Helps to avoid try catch blocks in there
+ * @param fn - function to enter a stage
+ */
 const asyncWrapper = (fn: any) => {
-  return function(ctx: ContextMessageUpdate, next: any) {
-    fn(ctx).catch((error: any) => {
+  return async function(ctx: ContextMessageUpdate, next: any) {
+    try {
+      await fn(ctx);
+    } catch (error) {
       logger.error(ctx, 'General error, %O', error);
       ctx.reply('An error has occured... Please, try again');
       return next();
-    });
+    }
   };
 };
 
