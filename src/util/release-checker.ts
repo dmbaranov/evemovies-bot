ec9b487465acb9633f6247176c2a13e48927622e
Dmitrii Baranov <me@dmbaranov.io>
2018-12-11 18:58:29 +0200

    Added filmopotok torrent, but not in use

--- src/util/release-checker.ts
+++ src/util/release-checker.ts
@@ -21,3 +21,37 @@ export async function checkMovieRelease(imdbid: string): Promise<Boolean> {
 
   return torrents.items && torrents.items.length > 0;
 }
+
+export async function checkRussianMovieRelease(title: string, year: string) {
+  // For the future purposes... If there will be a day when I'll use filmopotok.ru for russian torrents
+  // As for now, I can't determine whether the torrents has a good or bad quality (e.g. ad, etc)
+  // Use this values to determine whether the quality "seems" to be good or not
+  // translation >= 25
+  // quality >= 12
+  logger.debug(undefined, 'Checking russian release for movie %s', title);
+
+  const url = encodeURI(`http://filmpotok.ru/search/autocomplete/all/${title}`);
+  let response;
+
+  try {
+    response = await rp.get(url);
+  } catch (e) {
+    return false;
+  }
+
+  let movieUrl = undefined;
+  const torrents = JSON.parse(response)[1];
+  for (let key in torrents) {
+    if (
+      torrents[key].label.match(/<i>(.*?)<\/i>/)[1].toLowerCase() === title.toLowerCase() &&
+      torrents[key].label.match(/> \((\d{4})/)[1] === year
+    ) {
+      movieUrl = torrents[key].href;
+      break;
+    }
+  }
+
+  if (!movieUrl) return false;
+
+  console.log(movieUrl);
+}
